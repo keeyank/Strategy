e69e9637b21986d81507e9cf2356ddbd8069e56e
Keeyan Kazemi <kkazemi@sfu.ca>
2018-12-12 14:52:30 -0800

    Revert "Updates to SetValidTiles"
    This reverts commit 0d199b7eb0f5cc923bd8b8e33c5887152b2fbecd.

--- Assets/Scenes/Tile.cs
+++ Assets/Scenes/Tile.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 public class Tile : MonoBehaviour {
 
@@ -11,8 +10,7 @@ public class Tile : MonoBehaviour {
 
     public TileMap tileMap;
 
-    public bool isValidMovement = false;
-    public bool isValidAttack = false;
+    public bool isValid = false;
 
     public int type; 
     public int x;
@@ -20,9 +18,8 @@ public class Tile : MonoBehaviour {
 
     public const int MAT_GRASS_INVALID = 0;
     public const int MAT_GRASS_VALID_MOVEMENT = 1;
-    public const int MAT_ROCK_INVALID = 2;
+    public const int MAT_ROCK = 2;
     public const int MAT_GRASS_VALID_ATTACK = 3;
-    public const int MAT_ROCK_VALID = 4;
 
     [SerializeField] // So we can add in the mats in the inspector
     public Material[] mats;
@@ -37,45 +34,25 @@ public class Tile : MonoBehaviour {
             gameObject.GetComponent<Renderer>().material = mats[MAT_GRASS_INVALID];
         }
         else if (type == ROCK) {
-            gameObject.GetComponent<Renderer>().material = mats[MAT_ROCK_INVALID];
+            gameObject.GetComponent<Renderer>().material = mats[MAT_ROCK];
         }
     }
 
-    // Assertion: isValidAttack and isValidMovement can never be true at the same time for any tile
-    public void setAsValidMovement() {
-        Assert.IsTrue(isValidAttack == false);
+    public void setAsValid(int validType) {
         if (type != GRASS) {
             return; // only grass can be a valid tile
         }
-        isValidMovement = true;
-        gameObject.GetComponent<Renderer>().material = mats[MAT_GRASS_VALID_MOVEMENT];
-    }
-
-    public void setAsValidAttack() {
-        Assert.IsTrue(isValidMovement == false);
-        isValidAttack = true;
-        if (type == GRASS) {
-            gameObject.GetComponent<Renderer>().material = mats[MAT_GRASS_VALID_ATTACK];
-        }
-        else if (type == ROCK) {
-            gameObject.GetComponent<Renderer>().material = mats[MAT_ROCK_VALID];
-        }
+        isValid = true;
+        gameObject.GetComponent<Renderer>().material = mats[validType];
     }
 
     public void setAsInvalid() {
-        Assert.IsTrue(!(isValidAttack && isValidMovement));
-        isValidMovement = false;
-        isValidAttack = false;
-        
-        if (type == GRASS) {
-            gameObject.GetComponent<Renderer>().material = mats[MAT_GRASS_INVALID];
+        if (type != GRASS) {
+            return;
         }
-        else if (type == ROCK) {
-            gameObject.GetComponent<Renderer>().material = mats[MAT_ROCK_INVALID];
-        }
-       
+        isValid = false;
+        gameObject.GetComponent<Renderer>().material = mats[MAT_GRASS_INVALID];
     }
-
 
     void OnMouseUp() {
         tileMap.processTilePress(x, y);
--- Assets/Scenes/Unit.cs
+++ Assets/Scenes/Unit.cs
@@ -8,13 +8,7 @@ public abstract class Unit : MonoBehaviour {
     public TileMap tileMap;
 
     public int speed = 5;
-    public int attack1Range = 1;
-    public int attack1Buffer = 0;
-    public int attack2Range;
-    public int attack2Buffer;
-    public int attack3Range;
-    public int attack3Buffer;
-
+    public int attackRange = 1;
     public int x = -1;
     public int y = -1;
 
